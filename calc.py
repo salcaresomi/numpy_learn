820b03a47a68a7efe0651938576198cb8a1367a2
salcaresomi <159338952+salcaresomi@users.noreply.github.com>
2024-02-09 13:46:38 +0100

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,11 @@
+x = [2, 3, 4, 5, 6]
+y = [a + 2 for a in x]
+
+import numpy as np
+nums = np.array([2, 3, 4, 5, 6])
+nums2 = nums + 2
+
+import numpy as np
+x = [2, 3, 4, 5, 6]
+nums = np.array([2, 3, 4, 5, 6])
+type(nums)
