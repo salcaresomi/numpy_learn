7afe027147ebbc08467cfe53d3dcf194af22f692
salcaresomi <159338952+salcaresomi@users.noreply.github.com>
2024-02-12 16:48:26 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,16 @@
+import numpy as np
+
+# List comprehension method
 x = [2, 3, 4, 5, 6]
 y = [a + 2 for a in x]
 
-import numpy as np
+# Direct addition using NumPy array
 nums = np.array([2, 3, 4, 5, 6])
 nums2 = nums + 2
 
-import numpy as np
-x = [2, 3, 4, 5, 6]
-nums = np.array([2, 3, 4, 5, 6])
-type(nums)
+# Checking the type of the NumPy array
+nums_type = type(nums)
+
+print("Using list comprehension:", y)
+print("Using NumPy array:", nums2)
+print("Type of nums array:", nums_type)
